3874e1967f3c4032f5666e8d829f30baae899956
Matt Brown <gitmattb@gmail.com>
2023-06-25 23:17:49 -0700

    add parser

--- src/model/offlinestatspage.py
+++ src/model/offlinestatspage.py
@@ -1,13 +1,16 @@
 from dataclasses import dataclass
 from datetime import datetime
+from option import Option
 
 @dataclass
 class OfflineStatsPage:
     userid: int
     username: str
     rank: int
+    alliance_id: Option[int]
     tff: int
-    commander_id: int
-    commandchain_top_id: int
+    tff_type: str
+    commander_id: Option[int]
+    commandchain_top_id: Option[int]
     officers: list[int]
     user_last_online: datetime
--- src/pageparser/offlinestatpageparser.py
+++ src/pageparser/offlinestatpageparser.py
@@ -1,7 +1,8 @@
 from src.model.offlinestatspage import OfflineStatsPage
 
 from bs4 import BeautifulSoup
-from option import Result
+from datetime import datetime
+from option import Result, Option
 
 class OfflineStatPageParser:
     def __init__(self, parser: str = 'lxml') -> None:
@@ -15,8 +16,75 @@ class OfflineStatPageParser:
         if content is None or soup.title is None:
             return Result.Err("Error parsing html")
         
+        if "Stats for" not in soup.title.text:
+            return Result.Err(f"Page has unexpected title {soup.title.text}")
+        
         return OfflineStatPageParser._extract_pagemodel_from_content(content)
     
     [staticmethod]
     def _extract_pagemodel_from_content(contentsoup: BeautifulSoup) -> Result[OfflineStatsPage, str]:
-        return Result.Err("ooof")
+        is_online = contentsoup.find('div', {"class": "playercard_name oindicator"}) is not None
+        
+        playerstats = OfflineStatPageParser._extract_playerstats(contentsoup)
+        
+        if playerstats.is_err:
+            return Result.Err(playerstats.Err())
+        
+        rank, alliance, tff, tff_type = playerstats.Ok()
+        if is_online:
+            last_online = datetime.now()
+        else:
+            pass
+        return Result.Ok(
+            OfflineStatsPage(
+                user_id, user_name, rank, alliance, tff, tff_type, commander_id, commanderchain_top_id, officers, last_online))
+    
+    [staticmethod]
+    def _extract_playerstats(contentsoup: BeautifulSoup) -> Result[tuple(int, Option(int), str, str), str]:
+        statssoup = contentsoup.find('div', {"class": "playercard_stats"})
+        rank = int(statssoup.find("div", {"class": "playercard_rank"}).text[1:])
+        tff, tff_type = statssoup.find("div", {"class": "playercard_size"}).text.split(' ')
+        tff = int(tff.replace(',', ''))
+        
+        alliancesoup = statssoup.find("div", {"class": "playercard_alliance"})
+        alliancelinksoup = alliancesoup.find("a")
+        
+        if alliancelinksoup is None:
+            alliance = Option.NONE()
+        else:
+            alliancelink = alliancelinksoup.get("href")
+            allianceid = int(alliancelink.split("?a=")[1])
+            alliance = Option.Some(allianceid)
+        
+        return Result.Ok(rank, alliance, tff, tff_type)
+        
+    [staticmethod]
+    def _extract_playercommandchain(contentsoup: BeautifulSoup) -> Result(tuple(Option(int), Option(int), list[int])):
+        
+        commandsoup = contentsoup.find("div", {"class": "playercard_commandchain"})
+        top = commandsoup.find("div", {"class": "playercard_topofchain"})
+        if top is None:
+            topid = Option.NONE()
+        else:
+            tophref = top.find("a").get("href")
+            top_idval = int(tophref.split("?a=")[1])
+            topid = Option.Some(top_idval)
+        
+        commandersoup = commandsoup.find("div", {"class": "playercard_commander"})
+        
+        if commandersoup is not None:
+            commander_id = Option.NONE()
+        else:
+            commanderhref = commandersoup.find("a").get("href")
+            commanderid_val = int(commanderhref.split("?a=")[1])
+            commander_id = Option.Some(commanderid_val)
+        
+        officersoup = commandsoup.find("div", {"class": "players"})
+        
+        if officersoup is None:
+            officers = []
+        else:
+            officers_soup = officersoup.find_all("li", {"class": "player_cell"})
+            officers = [int(x.get("id").split('_')[1]) for x in officers_soup]
+            
+        return Result.Ok(tuple(topid, commander_id, officers))
